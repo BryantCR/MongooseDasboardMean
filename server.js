d47cca88864aa4fdc6da5a45e401fa5ffacbadf5
BryantCR <bryancvargas20@gmail.com>
2021-11-22 18:45:44 -0600

    ok

--- server.js
+++ server.js
@@ -17,3 +17,42 @@ app.get( '/', function( request, response ){
             response.render( 'index', { users : data } );
         });  
 });
+
+app.get( '/', function( request, response ){
+    AnimalModel
+        .getAnimals()
+        .then( data => {
+            console.log( data );
+            response.render( 'index', { users : data } );
+        });  
+});
+
+app.post( '/animals/new', function( request, response ){
+    console.log( request.body );
+    const animalId = Number(request.body.animalId);
+    const animalName = request.body.animalName;
+    const animalInformation = request.body.animalInformation;
+
+    // Run validations to see if the 'id' is not already in the list
+    const newAnimal = {
+        animalId,
+        animalName,
+        animalInformation
+    };
+    console.log("Data from the form: " + newAnimal );
+    AnimalModel
+        .createUser( newAnimal )
+        .then( result => {
+            console.log("Result Catch: " + result );
+        })
+        .catch( err => {
+            console.log( "Something went wrong!" );
+            console.log( err );
+        })
+
+    response.redirect( '/' );
+});
+
+app.listen( 8080, function(){
+    console.log( "The users server is running in port 8080." );
+});
